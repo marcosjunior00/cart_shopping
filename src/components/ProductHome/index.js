cc464064db5ee45be87eedb37cf6cdcaa7020f1b
Marcos Junior <marcosantonio.jose01@gmail.com>
2023-07-13 19:49:49 -0300

    fix: init addToCart fix

--- src/components/ProductHome/index.js
+++ src/components/ProductHome/index.js
@@ -5,7 +5,6 @@ import formatCurrency from "../../utils/formatCurrency";
 
 const ProductHome = ({ data }) => {
   const { setCartItems, cartItems } = useContext(AppContext);
-  data = [{ product: data }];
 
   const addCartItem = () => {
     setCartItems([...cartItems, data]);
@@ -15,28 +14,26 @@ const ProductHome = ({ data }) => {
     <C.Container>
       <C.Thumb>
         <C.Img
-          src={data.product.thumbnail.replace(/\w\.jpg/gi, "W.jpg")}
+          src={data.thumbnail.replace(/\w\.jpg/gi, "W.jpg")}
           width="400px"
         />
       </C.Thumb>
 
       <C.InfoProduct>
-        <C.Title>{data.product.title}</C.Title>
+        <C.Title>{data.title}</C.Title>
 
-        <C.Price>{formatCurrency(data.product.price, "BRL")}</C.Price>
+        <C.Price>{formatCurrency(data.price, "BRL")}</C.Price>
         <p>
           em{" "}
           <C.Portion>
-            10X {formatCurrency(data.product.price / 10, "BRL")} sem juros
+            10X {formatCurrency(data.price / 10, "BRL")} sem juros
           </C.Portion>
         </p>
         <C.Link href="">Ver formas de pagamento</C.Link>
       </C.InfoProduct>
 
       <C.Details>
-        <C.Shipping
-          className={`${data.product.shipping.free_shipping ? "free" : ""}`}
-        >
+        <C.Shipping className={`${data.shipping.free_shipping ? "free" : ""}`}>
           <C.Icon className="bx bxs-truck"></C.Icon>
           <C.ShippingGroup>
             {data.shipping.free_shipping ? (
@@ -54,16 +51,13 @@ const ProductHome = ({ data }) => {
         <C.Seller>
           <p>
             Vendido por
-            <C.Link href={data.product.seller.permalink} target="_blank">
+            <C.Link href={data.seller.permalink} target="_blank">
               {data.seller.nickname}
             </C.Link>
           </p>
-          {data.product.seller.seller_reputation.metrics.sales.completed >
-          50 ? (
-            data.product.seller.seller_reputation.metrics.sales.completed >
-            200 ? (
-              data.product.seller.seller_reputation.metrics.sales.completed >
-              1000 ? (
+          {data.seller.seller_reputation.metrics.sales.completed > 50 ? (
+            data.seller.seller_reputation.metrics.sales.completed > 200 ? (
+              data.seller.seller_reputation.metrics.sales.completed > 1000 ? (
                 <p>+1000 vendas</p>
               ) : (
                 <p>-1000 vendas</p>
@@ -80,9 +74,9 @@ const ProductHome = ({ data }) => {
 
         <C.Stock>
           <C.StockTitle>Estoque disponível</C.StockTitle>
-          {data.product.sold_quantity === 1 ? (
+          {data.sold_quantity === 1 ? (
             <span>Último disponível!</span>
-          ) : data.product.sold_quantity === 0 ? (
+          ) : data.sold_quantity === 0 ? (
             <span className="off">Esgotado</span>
           ) : (
             <span>{data.sold_quantity} unidades</span>
@@ -90,7 +84,7 @@ const ProductHome = ({ data }) => {
         </C.Stock>
 
         <C.MercadoPagoCont>
-          {data.product.accepts_mercadopago ? (
+          {data.accepts_mercadopago ? (
             <>
               <C.Img
                 src="https://logospng.org/download/mercado-pago/logo-mercado-pago-icone-1024.png"
